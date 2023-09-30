d33e42f694e956ced2704ca6695f0fb6ed827f4b
Valeriya <valeriya.khan@nu.edu.kz>
2023-09-30 18:27:19 +0000

    exps changed

--- exps.sh
+++ exps.sh
@@ -11,7 +11,7 @@ python main.py --experiment=CIFAR50 --scenario=class --brain-inspired --si --see
 python main.py --experiment=CIFAR50 --scenario=class --brain-inspired --si --seed=1 --pre-convE --freeze-convE --seed-to-ltag  --time --contexts=6 --reg-strength=100000000 --dg-prop=0.6 --active-classes=all-so-far --model-type=resnet --eval_per_context --z-dim=300
 
 # example of training the model for CIFAR100, our method, branch laten_match_dist_cycle
-python main.py --experiment=CIFAR50 --scenario=class --brain-inspired --seed=2 --pre-convE --freeze-convE --seed-to-ltag  --time --contexts=11 --active-classes=all-so-far --cycles=10 --model-type=resnet --model-type=resnet --eval_per_context
+# python main.py --experiment=CIFAR50 --scenario=class --brain-inspired --seed=2 --pre-convE --freeze-convE --seed-to-ltag  --time --contexts=11 --active-classes=all-so-far --cycles=10 --model-type=resnet --model-type=resnet --eval_per_context
 
 # example of training the model for MINI, our method, branch laten_match_dist_cycle
-python main.py --experiment=MINI --scenario=class --brain-inspired --seed=2 --pre-convE --freeze-convE --seed-to-ltag  --time --contexts=11 --active-classes=all-so-far --cycles=10 --model-type=resnet --z-dim=300 --model-type=resnet --eval_per_context
+# python main.py --experiment=MINI --scenario=class --brain-inspired --seed=2 --pre-convE --freeze-convE --seed-to-ltag  --time --contexts=11 --active-classes=all-so-far --cycles=10 --model-type=resnet --z-dim=300 --model-type=resnet --eval_per_context
